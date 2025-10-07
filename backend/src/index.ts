02c809163c50d01213630eef83b9a50446cc9d98
ddas4548 <ddas4548@gmail.com>
2025-10-07 16:13:53 +0530

    socket server setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,29 @@
 import express from "express";
 import http from "http";
 import serverConfig from "./config/serverConfig";
+import cors from 'cors';
+import { Server } from "socket.io";
 
 const app = express();
 
+app.use(cors())
+
+
 const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("New user connected");
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+});
 
 server.listen(serverConfig.PORT, () => {
     console.log(`Server is up at port ${serverConfig.PORT}`);
