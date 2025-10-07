abcf2071b25704f77863fb9f6b64f2b7ad3688e0
ddas4548 <ddas4548@gmail.com>
2025-10-07 21:16:07 +0530

    FE and BE connect through websocket

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,29 @@
+import cors from "cors";
 import express from "express";
 import http from "http";
-import serverConfig from "./config/serverConfig";
-import cors from 'cors';
 import { Server } from "socket.io";
+import serverConfig from "./config/serverConfig";
 
 const app = express();
 
-app.use(cors())
-
+app.use(cors());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    methods: ["POST", "GET"],
+    origin: "*",
+  },
 });
 
 io.on("connection", (socket) => {
-    console.log("New user connected");
-    socket.on("disconnect", () => {
-        console.log("User disconnected");
-    });
+  console.log("New user connected");
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
 });
 
 server.listen(serverConfig.PORT, () => {
-    console.log(`Server is up at port ${serverConfig.PORT}`);
+  console.log(`Server is up at port ${serverConfig.PORT}`);
 });
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+import { SocketProvider } from './Context/SocketContext.tsx'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <SocketProvider>
     <App />
-  </StrictMode>,
+  </SocketProvider>,
 )
